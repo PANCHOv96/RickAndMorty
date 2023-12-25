591a408731b4563580c7cee0b9fa9e8b4feefa42
Francisco Vallone <vallonefrancisco@gmail.com>
2023-12-25 09:05:46 -0300

    se crearon los componentes de los personajes y el personaje para poder mostrar la informacion en pantalla , tambien se agregaron svg para mostrar como iconos

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { useCallback, useState } from 'react';
 import useCharacters from './hooks/useCharacters';
 import Characters from './components/characters'
 import debounce from "just-debounce-it";
+import Error from './components/error';
+import Footer from './components/Footer/footer'
 
 function App() {
   const [search,setSearch] = useState('')
@@ -25,17 +27,18 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold text-red-500">
+      <h1 className="text-3xl font-semibold text-green-400 text-center py-5">
         RICK AND MORTY
       </h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={handleChange} value={search}/>
-        <input type="submit" value='Search'/>
+      <form onSubmit={handleSubmit} className='flex justify-center w-2/4 m-auto gap-1'>
+        <input type="text" onChange={handleChange} value={search} className="flex-1 border-0 rounded-lg py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"/>
+        <input type="submit" value='Search' className="rounded-md bg-red-200 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-400 hover:cursor-pointer "/>
       </form>
       {(!characters.error && characters) && <Characters characters={characters}/>}
       {characters.error && <>
-        {characters.error}
+        <Error msj={characters.error}/>
       </>}
+      <Footer />
     </>
   )
 }
--- src/components/characters.jsx
+++ src/components/characters.jsx
@@ -4,7 +4,7 @@ export default function Characters ({characters}){
     if (!characters) return
     return(
         <>
-            <div className="grid grid-cols-5 gap-4">
+            <div className="grid lg:grid-cols-5 gap-4 md:grid-cols-3 mx-2 lg:mx-0 py-4">
                 {characters && characters.map(character => {
                     return(
                         <Character character={character} key={character.id}/>
